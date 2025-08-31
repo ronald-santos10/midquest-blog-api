5e3dd7ca342ec7eeb56107ba5a7d921e45748190
ronald-santos10 <ronalddesign10@gmail.com>
2025-08-31 16:52:38 -0300

    fix database

--- config/database.ts
+++ config/database.ts
@@ -1,28 +1,22 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 0),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
-  };
+  const client = env('DATABASE_CLIENT');
 
   return {
-    connection: connections[client],
+    connection: {
+      client: client === 'postgres' ? 'postgres' : 'sqlite',
+      ... (client === 'postgres' ? {
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+      } : {
+        connection: {
+          filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      })
+    },
   };
 };
