a207d52a0d55f33af2538b8a5e49f23b5599428e
ronald-santos10 <ronalddesign10@gmail.com>
2025-09-04 13:14:07 -0300

    fix email-send

--- src/api/post/content-types/post/lifecycles.ts
+++ src/api/post/content-types/post/lifecycles.ts
@@ -1,6 +1,5 @@
 import SibApiV3Sdk from "sib-api-v3-sdk";
 
-// Configuração do cliente Brevo
 const client = SibApiV3Sdk.ApiClient.instance;
 const apiKey = client.authentications["api-key"];
 apiKey.apiKey = process.env.BREVO_API_KEY as string;
@@ -9,32 +8,34 @@ const site = process.env.SITE_DOMAIN_BLOG as string;
 const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const lifecycles = {
-  async afterCreate(event: any) {
-    strapi.log.info("🚀 Lifecycle afterCreate disparado");
-    strapi.log.info(event.result);
-    const { result } = event;
+  async afterUpdate(event: any) {
+    const { result, previousResult } = event;
 
-    const listId = 3;
-    const templateId = 1;
+    if (result.publishedAt && !previousResult.publishedAt) {
+      strapi.log.info("🚀 Post publicado, disparando email...");
 
-    const sendSmtpEmail: SibApiV3Sdk.SendSmtpEmail = {
-      sender: { email: process.env.EMAIL_FROM, name: "Midquest" },
-      templateId,
-      listIds: [listId],
-      params: {
-        title: result.title,
-        description: result.description,
-        link: `${site}/${result.slug}`,
-      },
-    };
+      const listId = 3;
+      const templateId = 1;
 
-    try {
-      await tranEmailApi.sendTransacEmail(sendSmtpEmail);
-      strapi.log.info(
-        `📧 Email via template ${templateId} enviado para lista ${listId}`
-      );
-    } catch (err) {
-      strapi.log.error("Erro ao enviar email:", err);
+      const sendSmtpEmail: SibApiV3Sdk.SendSmtpEmail = {
+        sender: { email: process.env.EMAIL_FROM, name: "Midquest" },
+        templateId,
+        listIds: [listId],
+        params: {
+          title: result.title,
+          description: result.description,
+          link: `${site}/${result.slug}`,
+        },
+      };
+
+      try {
+        await tranEmailApi.sendTransacEmail(sendSmtpEmail);
+        strapi.log.info(
+          `📧 Email via template ${templateId} enviado para lista ${listId}`
+        );
+      } catch (err) {
+        strapi.log.error("Erro ao enviar email:", err);
+      }
     }
   },
 };
